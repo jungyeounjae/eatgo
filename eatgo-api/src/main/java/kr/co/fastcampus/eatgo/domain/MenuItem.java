43aed326bd0719faa45c7536fa7933bb528bb113
yeounjae <duswp220@gmail.com>
2020-05-18 22:22:49 +0900

    commit test

--- eatgo-api/src/main/java/kr/co/fastcampus/eatgo/domain/MenuItem.java
+++ eatgo-api/src/main/java/kr/co/fastcampus/eatgo/domain/MenuItem.java
@@ -1,7 +1,7 @@
 package kr.co.fastcampus.eatgo.domain;
 
 public class MenuItem {
-    private final String name; //ffff
+    private final String name; //ffffdwdw
 
     public MenuItem(String name) {
         this.name = name;
