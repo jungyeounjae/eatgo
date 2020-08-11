2088ea138d94d6a9d2f9b8f2c1fdf90ca24895bc
jungyeounjae <duswp220@naver.com>
2020-08-12 03:06:06 +0900

    validate  주석 추가

--- eatgo-api/src/main/java/kr/co/fastcampus/eatgo/interfaces/RestaurantErrorAdvice.java
+++ eatgo-api/src/main/java/kr/co/fastcampus/eatgo/interfaces/RestaurantErrorAdvice.java
@@ -7,13 +7,14 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ControllerAdvice
+@ControllerAdvice // 전역
 public class RestaurantErrorAdvice {
 
     @ResponseBody
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    @ExceptionHandler(RestaurantNotFoundException.class)
+    @ExceptionHandler(RestaurantNotFoundException.class) // @Controller Bean내에서 발생하는 예외를 처리해주는 역활. 인자로서 캐치하고 싶은 예외 클래스를 지정
     public String handleNotFound() {
+        // RestaurantNotFoundException 이 발생할 때 쓰는 에러 핸들러
         return "{}";
     }
 
